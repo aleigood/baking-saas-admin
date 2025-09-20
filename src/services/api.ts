b3a8801b533ddd8c5d2f93961c714d7a1251b0e4
aleigood <aleigood@gmail.com>
2025-09-20 12:58:59 +0800

    编辑器导入配方应用预定义原料属性

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:3000", // 您的后端 API 地址
+    baseURL: "http://localhost:9527", // 您的后端 API 地址
 });
 
 // 请求拦截器，用于在每个请求头中添加 JWT
